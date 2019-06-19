e324051bc91fb39425a98e90a68ce8796086ddfc
Chris Lamb <chrislamb820@gmail.com>
2019-06-19 16:02:59 +1000

    For Webreader version 3.1

--- dss_readspeaker/rsconf.js
+++ dss_readspeaker/rsconf.js
@@ -1,4 +1,4 @@
 // These configurations prevent the readspeaker player:
-// 1. from detaching and following the scroll down the page.
-// 2. including a link to download an audio file of the page contents
-window.rsConf = { ui: { disableDetachment: true, displayDownload: false }};
+// 1. from displaying Google translations
+// 2. from displaying a dictionary look up option
+window.rsConf = { ui: { tools: {translation: false, dictionary: false}}};
